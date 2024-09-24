0d78af1db5012cfab631d91af441c6525282408d
Брусенцев Александр Юрьевич <abrusentsev@aviatron.ru>
2024-09-24 16:49:42 +0500

    uart receive and transmission in formatted way were added

--- Source/main.c
+++ Source/main.c
@@ -14,9 +14,10 @@ Purpose : Generic application start
 #include <stdlib.h>
 #include <RTT/SEGGER_RTT.h>
 #include "hardware.h"
+#include <string.h>
+#include <stdbool.h>
 
-static void DMA1_TransmitComplete_Callback(void);
-static void DMA1_ReceiveComplete_Callback(void);
+
 
 typedef struct
 { 
@@ -27,14 +28,22 @@ typedef struct
     float humidity;
 }host__data_exchange_t;
 
+
 host__data_exchange_t to_host = 
 {
-    .outputs = 0xDEAD,
+    .outputs = 0x000D,
     .inputs = 0xBEAF,
     .temperature = -20.0f,
     .pressure = 100.3f,
     .humidity = 70.3f
 };
+
+static void DMA1_TransmitComplete_Callback(void);
+static void DMA1_ReceiveComplete_Callback(void);
+static char* GetFormattedHostData(const host__data_exchange_t* data);
+bool TransmittionIsComleted = true;
+
+
 
 host__data_exchange_t from_host;
 
@@ -51,21 +60,26 @@ int main(void)
         &from_host, 
         sizeof(from_host),
         &DMA1_ReceiveComplete_Callback
-    );
-    
+    );    
 
     Configure_DMA_Transmitter(
-        &to_host, 
-        sizeof(to_host), 
+        GetFormattedHostData(&to_host), 
+        strlen(GetFormattedHostData(&to_host)), 
         &DMA1_TransmitComplete_Callback
     );
-    
+    StartReception();
     
     while(1)
-    {        
+    {       
+        while(!TransmittionIsComleted)
+        {
+        }
+        
+        GetFormattedHostData(&to_host);   
+        TransmittionIsComleted = false;     
         StartTransmission();
         LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_13);
-        for (uint32_t i = 0; i < 10000000; ++i) { /*delay*/ }
+        for (uint32_t i = 0; i < 2000000; ++i) { /*delay*/ }
     }
 
 }
@@ -75,15 +89,26 @@ int main(void)
 
 static void DMA1_TransmitComplete_Callback(void)
 {
-    printf("transmission is completed\n");
+    TransmittionIsComleted = true;
     StopTransmission();
-
-
-
-
 }
 
 static void DMA1_ReceiveComplete_Callback(void)
 {
     printf("reception is completed\n");
+}
+
+
+static char* GetFormattedHostData(const host__data_exchange_t* data)
+{
+    static char out_buffer[160];
+    sprintf(out_buffer, 
+      "{ outputs = %#06X, inputs = %#06X, temperature = %07.2f, pressure = %07.2f, humidity = %07.2f }\n",      
+      data->outputs,
+      data->inputs,
+      data->temperature,
+      data->pressure,
+      data->humidity
+    );
+    return out_buffer;
 }
